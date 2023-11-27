b64fce93040407ca43316194a2e3289aa518641e
Luiz Fonseca <eu@luiz.cc>
2023-11-27 18:04:16 +0100

    feat: add the ability to create suffixed domains

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-echo ">> Loading environment variables from .env file"
+echo "$(tput setaf 4)>> Loading environment variables from .env file$(tput sgr0)"
 eval "$(
   cat .env | awk '!/^\s*#/' | awk '!/^\s*$/' | while IFS='' read -r line; do
     key=$(echo "$line" | cut -d '=' -f 1)
@@ -8,8 +8,16 @@ eval "$(
     echo "export $key=\"$value\""
   done
 )"
-echo "   DONE"
-echo ">> Deploying services"
+echo "$(tput setaf 4)>> Deploying services$(tput sgr0)"
 
 
-docker stack deploy -c main.compose.yml -c 0-agents.compose.yml -c 1-logging.compose.yml -c 2-dashboard.compose.yml -c 3-tracing.compose.yml -c 4-registry.compose.yml -c 5-swarm-management.compose.yml olc
+tput setaf 6 && docker stack deploy -c main.compose.yml -c 0-agents.compose.yml -c 1-logging.compose.yml -c 2-dashboard.compose.yml -c 3-tracing.compose.yml -c 4-registry.compose.yml -c 5-swarm-management.compose.yml olc
+
+
+
+echo " "
+echo "$(tput setaf 2)The following services URLs were created by default:$(tput sgr0)"
+echo " - $(tput setaf 3)https://portainer$DOMAIN_SUFFIX.$DOMAIN_NAME$(tput sgr0) (Stack Management)"
+echo " - $(tput setaf 3)https://grafana$DOMAIN_SUFFIX.$DOMAIN_NAME$(tput sgr0) (Monitoring, Logs, Traces)"
+echo " - $(tput setaf 3)https://registry$DOMAIN_SUFFIX.$DOMAIN_NAME$(tput sgr0) (Private Docker Registry)"
+echo " "
