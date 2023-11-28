971bac6e3e1b9ee69fc40c5d66452699ba0f88a9
Luiz Fonseca <eu@luiz.cc>
2023-11-28 03:02:42 +0100

    feat: use dex for docker metrics

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,38 +1,4 @@
 #! /bin/bash
-
-echo "$(tput setaf 1)IMPORTANT: $(tput sgr0)"
-echo " "
-echo "$(tput setaf 1)In order for container metrics to be scraped properly $(tput sgr0)"
-echo "$(tput setaf 1)we need to create a "socat" container that creates a TCP host:port $(tput sgr0)"
-echo "$(tput setaf 1)bind for the /var/run/docker.sock socket. $(tput sgr0)"
-
-echo " "
-echo " "
-echo " "
-read -n 1 -p "Confirm? (y/n):" "mainmenuinput"
-
-if [ "$mainmenuinput" = "y" ]; then
-  echo " "
-  echo "$(tput setaf 4)>> Creating socat container$(tput sgr0)"
-
-  # mkdir -p /var/docker-socat
-  docker service rm docker-metrics
-  docker service create \
-    -d \
-    --name docker-metrics \
-    --mode global \
-    --mount type=bind,source=/var/run/docker.sock,destination=/var/run/docker.sock \
-    --network host \
-    --publish 2376:2376 \
-    alpine/socat \
-    tcp-listen:2376,fork,reuseaddr unix-connect:/var/run/docker.sock
-
-  echo "$(tput setaf 4)>> Socat container created $(tput sgr0)"
-else
-  echo "$(tput setaf 1)Aborting$(tput sgr0)"
-  exit 1
-fi
-
 echo "$(tput setaf 4)>> Loading environment variables from .env file$(tput sgr0)"
 eval "$(
   cat .env | awk '!/^\s*#/' | awk '!/^\s*$/' | while IFS='' read -r line; do
