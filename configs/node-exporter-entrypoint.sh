547fff861d55e0fbaf75df378b35fe1d89c84c58
Luiz Fonseca <eu@luiz.cc>
2024-06-09 02:25:07 +0200

    feat: replace caddy with proksi

--- configs/node-exporter-entrypoint.sh
+++ configs/node-exporter-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -e
 
+NODE_NAME=$(cat /etc/nodename) # requires the mount on /etc/hostname:/etc/nodename
+
 echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /home/node-meta.prom
 
 set -- /bin/node_exporter "$@"
